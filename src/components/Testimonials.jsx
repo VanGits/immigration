0432cb31e1e813966b8da999c3b385747bac4714
Van Celdran <celdranvan@gmail.com>
2022-08-01 16:58:10 -0700

    d

--- src/components/Testimonials.jsx
+++ src/components/Testimonials.jsx
@@ -4,6 +4,7 @@ import forbes from "../assets/forbes-logo.svg";
 import huff from "../assets/huffpost-logo.svg";
 import inc from "../assets/inc-logo.svg";
 import entre from "../assets/entrepreneur-logo.svg";
+import jeff from "../assets/jeff.jpg";
 
 const Testimonials = () => {
   return (
@@ -28,8 +29,35 @@ const Testimonials = () => {
           <div className="testimonals__others">
             <h4>WHAT OTHERS ARE SAYING</h4>
           </div>
-          <div className="testimonals__comments">
-            
+
+          <div className="comments__wrapper">
+            <div className="testimonals__comments">
+              <img src={jeff} alt="" />
+              <p id="testimonials-desc">
+                Brian’s SEO knowledge is insane. If you want higher rankings,
+                you need to read his stuff – he’s the Unicorn among a sea of
+                donkey SEOs.
+              </p>
+              <div>
+                <h6>Jeff Bezos </h6>
+                <h6>·</h6> <h5> Amazon</h5>
+              </div>
+            </div>
+          </div>
+          <div className="comments__wrapper">
+            <div className="testimonals__comments">
+              <img src={jeff} alt="" />
+              <p id="testimonials-desc">
+                Lorem ipsum, dolor sit amet consectetur adipisicing elit.
+                Inventore, esse nesciunt. Quidem, commodi quasi magni neque qui
+                amet? Numquam magnam non expedita iure hic corporis pariatur
+                suscipit quaerat et? Officiis.
+              </p>
+              <div>
+                <h6>Larry Kim </h6>
+                <h6>·</h6> <h5>WordStream</h5>
+              </div>
+            </div>
           </div>
         </div>
       </div>
