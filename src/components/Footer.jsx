5a085f0c1e3d3d7a7ce2511863992e1910f62241
Van Celdran <celdranvan@gmail.com>
2022-08-02 15:50:53 -0700

    d

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -4,7 +4,10 @@ import "../styles/Footer.css"
 const Footer = () => {
     return (
         <footer className='Footer'>
-            
+            <div className="footer__div">
+                <h1>LAKBAY</h1>
+                <p>Next-level SEO training and link building strategies</p>
+            </div>
         </footer>
     );
 }
