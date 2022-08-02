f6afb48b8b3d01c10adf6ee80c18d13ff436b892
Van Celdran <celdranvan@gmail.com>
2022-08-01 17:20:15 -0700

    d

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import './App.css';
 import Nav from "./components/Nav.jsx"
 import Landing from "./components/Landing.jsx"
 import Testimonials from './components/Testimonials';
+import Featured from './components/Featured';
 
 function App() {
   return (
@@ -10,6 +11,7 @@ function App() {
       <Nav/>
       <Landing/>
       <Testimonials/>
+      <Featured/>
       
     </div>
   );
---
+++ src/components/Featured.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import "../styles/Featured.css";
+import wave from "../assets/div-wave.svg"
+
+const Featured = () => {
+    return (
+        <div className='Featured'>
+            <img src={wave} alt="" />
+           
+        </div>
+    );
+}
+
+export default Featured;
